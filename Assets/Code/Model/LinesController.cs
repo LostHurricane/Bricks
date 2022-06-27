a7c46ac9e15715aab9420df4e99542b65cec86f6
LostHurricane <kulich93@gmail.com>
2022-06-28 01:01:10 +0300

    Lines

--- Assets/Code/Model/LinesController.cs
+++ Assets/Code/Model/LinesController.cs
@@ -9,7 +9,6 @@ namespace Bricks
     {
         private Transform _parent;
 
-        private List <LineRenderer> _lineRenderers;
         private List<Pair> _connectedObjects;
         private MouseClicker _mouseClicker;
 
@@ -25,6 +24,7 @@ namespace Bricks
 
         public void Execute()
         {
+            StateCheck();
 
             if (_mouseClicker.RightClick)
             {
@@ -36,7 +36,10 @@ namespace Bricks
                     }
                     else
                     {
-                        _connectedObjects.Add(new Pair{elementOne = _lastElemeent, elementTwo = element});
+                        var lr = new GameObject().AddComponent<LineRenderer>();
+                        lr.gameObject.transform.SetParent(_parent, false);
+                        _connectedObjects.Add(new Pair{elementOne = _lastElemeent, elementTwo = element, lineRenderer = lr });
+                        
                         Debug.Log("AddedIt");
 
                     }
@@ -46,22 +49,31 @@ namespace Bricks
                     _lastElemeent = null;
                 }
             }
-            StateCheck();
         }
 
         private void StateCheck ()
         {
+            foreach (var pair in _connectedObjects)
+            {
+                if (!pair.elementOne || !pair.elementTwo)
+                {
+                    Object.Destroy(pair.lineRenderer.gameObject);
+                }
+            }
             _connectedObjects.RemoveAll(pair => !pair.elementOne || !pair.elementTwo);
             Debug.Log(_connectedObjects.Count);
 
 
-        
+            
+
         }
 
         private struct Pair
         {
             public BuildingElement elementOne;
             public BuildingElement elementTwo;
+
+            public LineRenderer lineRenderer;
         }
     }
 }
