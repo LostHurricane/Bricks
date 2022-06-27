00415e2fd65bfa6a31639c00ff491d77f6abe732
LostHurricane <kulich93@gmail.com>
2022-06-27 23:53:23 +0300

    Update MouseClicker.cs

--- Assets/Code/Model/MouseClicker.cs
+++ Assets/Code/Model/MouseClicker.cs
@@ -9,6 +9,7 @@ namespace Bricks
         private const float DOUBLE_CLICK_TIME = 0.3f;
 
         public bool LeftClick => Input.GetMouseButtonDown(0);
+        public bool RightClick => Input.GetMouseButtonDown(1);
         public bool DoubleClick { get; private set; }
 
         private float _lastClickTime;
